2a5673932adaaec8420b977afae943ed5d9a48e0
Kylie Rasku, RN <58571706+krashrasku@users.noreply.github.com>
2020-01-30 19:48:05 -0500

    Update malesOnly.sql

--- malesOnly.sql
+++ malesOnly.sql
@@ -1,5 +1,7 @@
-/* Select only MALES : Note, there are 4377 males in our DB */
+/* Select only MALES : Note, there are 4377 males in our DB (gender_source_value = 1)*/
+/* There are also 5623 FEMALES and 0 persons identified in the DB as neither MALE nor FEMALE */
 
 SELECT * FROM person, concept
 WHERE person.gender_concept_id = concept.concept_id
 AND concept.concept_name = 'MALE'
+
