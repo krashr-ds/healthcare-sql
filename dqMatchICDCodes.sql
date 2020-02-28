cb1cf44a1952c96e1f8b3dce974a88d1a94e807a
Kylie R. RN <58571706+krashrasku@users.noreply.github.com>
2020-02-28 13:10:31 -0500

    Create dqMatchICDCodes.sql

---
+++ dqMatchICDCodes.sql
@@ -0,0 +1,15 @@
+* Validate Concept Codes: ICD9CM, ICD9Proc, ICD10CM
+   This is basic format validation; this does not ensure codes are in the code database.
+   NOTE: Install & load /usr/lib/sqlite3/pcre.so before attempting to execute.
+   Regular Expressions Debugged here: https://regex101.com/
+   :) !
+   Kyle P. Rasku RN
+*/
+
+SELECT SUM(total_ICDcode_rows.bad_code) as badcode_count, (1.0*SUM(total_ICDcode_rows.bad_code)/SUM(total_ICDcode_rows.row_count)) AS percent_bad FROM 
+(
+SELECT 1 AS row_count, CASE WHEN concept_code REGEXP '([A-TV-Z|a-tv-z|\d])([\d])([A-TV-Z|a-tv-z|\d])(.?)([A-TV-Z|a-tv-z|\d]{0,4})' THEN 0 ELSE 1 END AS bad_code 
+FROM concept
+WHERE vocabulary_id IN ('ICD9CM', 'ICD9Proc', 'ICD10CM')
+AND invalid_reason IS NULL 
+) total_ICDcode_rows
