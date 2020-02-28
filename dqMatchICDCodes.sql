1ac81c02a0837ba2a618200fed50741d2cfad3f4
Kylie R. RN <58571706+krashrasku@users.noreply.github.com>
2020-02-28 13:33:27 -0500

    Update dqMatchICDCodes.sql
    Fixed to exclude Procedures, which may validate differently

--- dqMatchICDCodes.sql
+++ dqMatchICDCodes.sql
@@ -1,7 +1,11 @@
-* Validate Concept Codes: ICD9CM, ICD9Proc, ICD10CM
+/* Validate Concept Codes: ICD9CM, ICD10CM; Exclude procedures 
+
    This is basic format validation; this does not ensure codes are in the code database.
+   
    NOTE: Install & load /usr/lib/sqlite3/pcre.so before attempting to execute.
+   
    Regular Expressions Debugged here: https://regex101.com/
+   
    :) !
    Kyle P. Rasku RN
 */
@@ -10,6 +14,7 @@ SELECT SUM(total_ICDcode_rows.bad_code) as badcode_count, (1.0*SUM(total_ICDcode
 (
 SELECT 1 AS row_count, CASE WHEN concept_code REGEXP '([A-TV-Z|a-tv-z|\d])([\d])([A-TV-Z|a-tv-z|\d])(.?)([A-TV-Z|a-tv-z|\d]{0,4})' THEN 0 ELSE 1 END AS bad_code 
 FROM concept
-WHERE vocabulary_id IN ('ICD9CM', 'ICD9Proc', 'ICD10CM')
+WHERE vocabulary_id IN ('ICD9CM', 'ICD10CM')
 AND invalid_reason IS NULL 
+AND domain_id = 'Condition'
 ) total_ICDcode_rows
