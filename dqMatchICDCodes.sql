ba0c4283f7992d471f8b73513c37c46058cfc3af
Kylie R. RN <58571706+krashrasku@users.noreply.github.com>
2020-02-28 13:47:35 -0500

    Update dqMatchICDCodes.sql
    Fix Regexp

--- dqMatchICDCodes.sql
+++ dqMatchICDCodes.sql
@@ -12,7 +12,7 @@
 
 SELECT SUM(total_ICDcode_rows.bad_code) as badcode_count, (1.0*SUM(total_ICDcode_rows.bad_code)/SUM(total_ICDcode_rows.row_count)) AS percent_bad FROM 
 (
-SELECT 1 AS row_count, CASE WHEN concept_code REGEXP '([A-TV-Z|a-tv-z|\d])([\d])([A-TV-Z|a-tv-z|\d])(.?)([A-TV-Z|a-tv-z|\d]{0,4})' THEN 0 ELSE 1 END AS bad_code 
+SELECT 1 AS row_count, CASE WHEN concept_code REGEXP '^([A-TV-Z|a-tv-z|\d])([\d])([A-TV-Z|a-tv-z|\d])(\.?)([A-TV-Z|a-tv-z|\d]{0,4})$' THEN 0 ELSE 1 END AS bad_code 
 FROM concept
 WHERE vocabulary_id IN ('ICD9CM', 'ICD10CM')
 AND invalid_reason IS NULL 
