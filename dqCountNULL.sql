62630d68a6c716a125b39b8c367bb77c1d2b6a75
Kylie R. RN <58571706+krashrasku@users.noreply.github.com>
2020-02-28 13:55:22 -0500

    Update dqCountNULL.sql
    They're not 'bad' because they're null!

--- dqCountNULL.sql
+++ dqCountNULL.sql
@@ -5,7 +5,7 @@
    (I picked preceding_visit_occurrence_id because I knew it had some NULLs.)
 */
 
-SELECT SUM(total_rows.null_row) as null_count, (1.0*SUM(total_rows.null_row)/SUM(total_rows.row_count)) AS percent_bad 
+SELECT SUM(total_rows.null_row) as null_count, (1.0*SUM(total_rows.null_row)/SUM(total_rows.row_count)) AS percent_null 
 FROM
 (
 	SELECT 1 AS row_count, (CASE WHEN v.preceding_visit_occurrence_id IS NULL THEN 1 ELSE 0 END) AS null_row FROM visit_occurrence v
