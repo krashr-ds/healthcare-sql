26112d6875637961547b574160b0c16ddc8c24cf
Kylie R. RN <58571706+krashrasku@users.noreply.github.com>
2020-03-18 12:11:05 -0400

    Update allHeartAttacks.sql
    Improved to include ICD10 and SNOMED codes

--- allHeartAttacks.sql
+++ allHeartAttacks.sql
@@ -1,5 +1,16 @@
-/* Get me all the Heart Attacks */
+/* Heart Attacks 
+   3/18/20 Revised & improved:
+           ICD9 41000 - 41093
+		   ICD10 I21*
+		   SNOMED 413444003 - Acute Myocardial Ischemia (disorder)
+		          57054005 - Acute MI (disorder)
+		   36144 rows returned in 35081ms
+*/
 
 SELECT * FROM condition_occurrence, concept
 WHERE condition_occurrence.condition_concept_id = concept.concept_id
-AND condition_status_source_value BETWEEN 41000 AND 41093
+AND (condition_status_source_value BETWEEN 41000 AND 41093
+     OR condition_status_concept_id = 57054005
+	 OR condition_status_source_value IN (413444003, 57054005)
+	 OR condition_status_source_value LIKE 'I21*')
+ORDER BY condition_status_source_value
